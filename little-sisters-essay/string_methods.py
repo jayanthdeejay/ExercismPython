e0b6be760c8a5daa3e1c734b45d6c0e5086fce2b
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-02-15 18:36:44 +0530

    finished exercise little-sisters-essay

--- little-sisters-essay/string_methods.py
+++ little-sisters-essay/string_methods.py
@@ -1,12 +1,13 @@
+"""
+Little Sister's Essay
+"""
 def capitalize_title(title):
     """
 
     :param title: str title string that needs title casing
     :return:  str title string in title case (first letters capitalized)
     """
-
-    pass
-
+    return title.title()
 
 def check_sentence_ending(sentence):
     """
@@ -14,9 +15,7 @@ def check_sentence_ending(sentence):
     :param sentence: str a sentence to check.
     :return:  bool True if punctuated correctly with period, False otherwise.
     """
-
-    pass
-
+    return bool(sentence.endswith('.'))
 
 def clean_up_spacing(sentence):
     """
@@ -24,9 +23,7 @@ def clean_up_spacing(sentence):
     :param sentence: str a sentence to clean of leading and trailing space characters.
     :return: str a sentence that has been cleaned of leading and trailing space characters.
     """
-
-    pass
-
+    return sentence.strip()
 
 def replace_word_choice(sentence, old_word, new_word):
     """
@@ -36,5 +33,4 @@ def replace_word_choice(sentence, old_word, new_word):
     :param new_word: str replacement word
     :return:  str input sentence with new words in place of old words
     """
-
-    pass
+    return sentence.replace(old_word, new_word)
