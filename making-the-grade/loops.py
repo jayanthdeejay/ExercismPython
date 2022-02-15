c4377e9a07e929a06e26550590a7af17dbb2afd1
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-02-15 18:04:59 +0530

    finished exercise making-the-grade

--- making-the-grade/loops.py
+++ making-the-grade/loops.py
@@ -1,19 +1,27 @@
+"""
+Making the Grade
+"""
+
 def round_scores(student_scores):
     """
     :param student_scores: list of student exam scores as float or int.
     :return: list of student scores *rounded* to nearest integer value.
     """
-
-    pass
-
+    rounded_scores = []
+    for score in student_scores:
+        rounded_scores.append(round(score))
+    return rounded_scores
 
 def count_failed_students(student_scores):
     """
     :param student_scores: list of integer student scores.
     :return: integer count of student scores at or below 40.
     """
-
-    pass
+    count = 0
+    for score in student_scores:
+        if score <= 40:
+            count += 1
+    return count
 
 
 def above_threshold(student_scores, threshold):
@@ -22,9 +30,11 @@ def above_threshold(student_scores, threshold):
     :param threshold :  integer
     :return: list of integer scores that are at or above the "best" threshold.
     """
-
-    pass
-
+    top_scores = []
+    for score in student_scores:
+        if score >= threshold:
+            top_scores.append(score)
+    return top_scores
 
 def letter_grades(highest):
     """
@@ -38,23 +48,31 @@ def letter_grades(highest):
              71 <= "B" <= 85
              86 <= "A" <= 100
     """
-
-    pass
-
+    interval = (highest - 40) / 4
+    threshold = [41]
+    for i in range(3):
+        threshold.append(round(threshold[i] + interval))
+    return threshold
 
 def student_ranking(student_scores, student_names):
     """
      :param student_scores: list of scores in descending order.
      :param student_names: list of names in descending order by exam score.
      :return: list of strings in format ["<rank>. <student name>: <score>"].
-     """
-
-    pass
-
+    """
+    ranking = []
+    for index, score in enumerate(student_scores):
+        rank = str(index + 1) + ". " + student_names[index] + ": " + str(score)
+        ranking.append(rank)
+    return ranking
 
 def perfect_score(student_info):
     """
     :param student_info: list of [<student name>, <score>] lists
     :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
     """
-    pass
+    for student in student_info:
+        if student[1] == 100:
+            return student
+    return []
+    
