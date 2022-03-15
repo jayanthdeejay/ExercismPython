8bce06d132fcc586f900a21853176be4206c1d48
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-03-16 01:57:49 +0530

    finished atbash_cipher. probably not efficient

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,34 @@
 def encode(plain_text):
-    pass
-
+    plain = "abcdefghijklmnopqrstuvwxyz"
+    cipher = "zyxwvutsrqponmlkjihgfedcba"
+    plain, cipher = list(plain), list(cipher)
+    key = dict(zip(plain, cipher))
+    del(plain)
+    cipher = ""
+    count = 0
+    plain_text = plain_text.lower()
+    for char in plain_text:
+        if char.isalnum():
+            if count == 5:
+                cipher += ' '
+                count = 0
+            if char.isdigit():
+                cipher += char
+            else:
+                cipher += key[char]
+            count += 1          
+    return cipher
 
 def decode(ciphered_text):
-    pass
+    plain = "abcdefghijklmnopqrstuvwxyz"
+    cipher = "zyxwvutsrqponmlkjihgfedcba"
+    plain, cipher = list(plain), list(cipher)
+    key = dict(zip(cipher, plain))
+    del(cipher)
+    plain = ""
+    for char in ciphered_text:
+        if char in key.keys():
+            plain += key[char]
+        if char.isdigit():
+            plain += char
+    return plain
