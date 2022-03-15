8ce2eafb8b902bf3a88dfb77f927e71fdeab0327
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-03-16 01:04:02 +0530

    Finished flatten-array exercise

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,2 +1,10 @@
 def flatten(iterable):
-    pass
+    flat_list = []
+    def flat(it):
+        for i in it:
+            if type(i) == list:
+                flat(i)
+            elif i != None:
+                flat_list.append(i)
+    flat(iterable)
+    return flat_list 
