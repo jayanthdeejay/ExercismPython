08230d23a3e0c367ce191f3e591a2f63a23d9c10
Jayanth Dungavath <jayanthdeejay@gmail.com>
2022-03-16 00:50:07 +0530

    Finished matching brackets online before my last commit

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -2,3 +2,13 @@ def is_paired(input_string):
     """Given a string containing brackets [], braces {}, parentheses (), 
     or any combination thereof, verify that any and all pairs are matched and nested correctly.
     """
+    stack = ['N']
+    dict = {']': '[', '}': '{', ')': '('}
+    for char in input_string:
+        if char in dict.keys():
+            if stack.pop() != dict[char]:
+                return False
+        elif char in dict.values():
+            stack.append(char)
+
+    return len(stack) == 1
